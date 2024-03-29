f47a341e394da61d5a0c32b049050125603d0a73
CYH <howardcyh@icloud.com>
2024-03-30 01:13:54 +0800

    新增fallElove SpringBoot 專案

---
+++ fallElove/src/main/java/com/example/fallelove/rental/RentalOrderDetails.java
@@ -0,0 +1,42 @@
+package com.example.fallelove.rental;
+
+public class RentalOrderDetails {
+
+    private Integer ROrdNo;
+    private Integer RNo;
+    private Double RPrice;
+    private Double RDesPrice;
+
+    public Integer getROrdNo() {
+        return ROrdNo;
+    }
+
+    public void setROrdNo(Integer ROrdNo) {
+        this.ROrdNo = ROrdNo;
+    }
+
+    public Integer getRNo() {
+        return RNo;
+    }
+
+    public void setRNo(Integer RNo) {
+        this.RNo = RNo;
+    }
+
+    public Double getRPrice() {
+        return RPrice;
+    }
+
+    public void setRPrice(Double RPrice) {
+        this.RPrice = RPrice;
+    }
+
+    public Double getRDesPrice() {
+        return RDesPrice;
+    }
+
+    public void setRDesPrice(Double RDesPrice) {
+        this.RDesPrice = RDesPrice;
+    }
+
+}
