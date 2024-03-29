8c3ed41443d5fe8d5af3c1f99dd64aa1c1f2d6dc
CYH <howardcyh@icloud.com>
2024-03-29 17:11:57 +0800

    新增fallElove SpringBoot 專案

---
+++ fallElove/src/main/java/com/example/fallelove/FallEloveApplication.java
@@ -0,0 +1,13 @@
+package com.example.fallelove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FallEloveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FallEloveApplication.class, args);
+	}
+
+}
---
+++ fallElove/src/test/java/com/example/fallelove/FallEloveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fallelove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FallEloveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
